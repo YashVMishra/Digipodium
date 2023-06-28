c1f2a00d62c770a440115fde62aa40e294cddba0
YashVMishra <yashvardhanmishra01@gmail.com>
2023-06-28 10:51:25 +0530

    first commit

---
+++ MERN Stack/Section-1/conditions.js
@@ -0,0 +1,14 @@
+num=344;
+
+if(num%2==0){
+    myvar=45;
+    var myvar1="Hello";
+    console.log("Even")
+    let myvar2="using let keyword";
+    const myvar3="great";
+} else {
+    console.log("Odd");
+}
+
+console.log(myvar3);
+
---
+++ MERN Stack/Section-1/index.js
@@ -0,0 +1,7 @@
+console.log("Hello JS");
+
+num=10;
+myName="Yash Vardhan Mishra"
+
+console.log(num);
+console.log(myName);
