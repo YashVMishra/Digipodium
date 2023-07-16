8395502f441d4ee0a4d2282bc34b0e041f8225e7
YashVMishra <yashvardhanmishra01@gmail.com>
2023-07-16 15:04:44 +0530

    React-Router-Dom Added

--- MERN Stack/Section-2/frontend/src/App.jsx
+++ MERN Stack/Section-2/frontend/src/App.jsx
@@ -1,16 +1,22 @@
+import { BrowserRouter, Routes, Route, Navigate, Link } from 'react-router-dom';
 import './App.css';
 import Home from './components/Home';
 import Login from './components/Login';
 import SignUp from './components/SignUp';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div>
-      <h1>
-        <Home></Home>
-        <Login></Login>
-        <SignUp></SignUp>
-      </h1>
+        <BrowserRouter>
+        <Navbar></Navbar>
+        <Routes>
+          <Route path='/' element={<Navigate to="/home"/>}></Route>
+          <Route path='home' element={<Home/>}/>
+          <Route path='login' element={<Login/>}/>
+          <Route path='signup' element={<SignUp/>}/>
+        </Routes>
+        </BrowserRouter>
     </div>
   );
 }
--- MERN Stack/Section-2/frontend/src/components/Home.jsx
+++ MERN Stack/Section-2/frontend/src/components/Home.jsx
@@ -4,6 +4,11 @@ const Home = () => {
   return (
     <div>
         <h1>HomePage</h1>
+        <p style={{fontSize : "50px", backgroundColor : "lightblue"}}>Inline CSS in JSX</p>
+
+        <p className='myclass'>We use className here in JSX</p>
+
+        <input type="text" />
     </div>
   )
 }
