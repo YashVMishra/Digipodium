13ec9a72075b158f53837920becef968a0445c3f
YashVMishra <yashvardhanmishra01@gmail.com>
2023-07-19 00:45:00 +0530

    Login and SignUp Added

--- MERN Stack/Section-2/frontend/src/components/Login.jsx
+++ MERN Stack/Section-2/frontend/src/components/Login.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import './login.css';
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div className='mybody'>
+
+      <div className="myCard">
+        <h2 className="form-title">Login Form</h2>
+
+        <form action="">
+          <label htmlFor="">E-Mail</label>
+          <input type="email" name="" id="" className="myinput" />
+          <label htmlFor="">Password</label>
+          <input type="password" name="" id="" className="myinput" />
+          <button className="mybtn">Login</button>
+        </form>
+
+      </div>
+  </div>
+
   )
 }
 
--- MERN Stack/Section-2/frontend/src/components/SignUp.jsx
+++ MERN Stack/Section-2/frontend/src/components/SignUp.jsx
@@ -1,8 +1,36 @@
 import React from 'react'
+import './signup.css';
+import './Digipodium logo.png';
 
 const SignUp = () => {
   return (
-    <div>SignUp</div>
+    <div className='mybody'>
+      <div className="mydiv">
+        <form action="">
+        
+          <img src="Digipodium logo.png" alt="" className="myimg" />
+          <h2 className="myh1">Sign Up For Free</h2>
+          
+          <label htmlFor="" className="mylabel">
+            Username
+          </label>
+          <input className="myinput" type="text" placeholder="Username" />
+
+          <label htmlFor="" className="mylabel">
+            E-mail Address
+          </label>
+          <input className="myinput" type="email" placeholder="E-mail Address" />
+
+          <label htmlFor="" className="mylabel">
+            Password
+          </label>
+          <input className="myinput" type="password" placeholder="Password" />
+
+          <button className="mybtn">Sign Up</button>
+        </form>
+      </div>
+
+    </div>
   )
 }
 
