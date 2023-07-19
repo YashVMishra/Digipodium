7b9eaa8d72c07ad7e5e87b84a3e51631fdb6cc04
YashVMishra <yashvardhanmishra01@gmail.com>
2023-07-19 13:45:42 +0530

    To-Do Files Added

--- MERN Stack/Section-2/frontend/src/components/Todo.jsx
+++ MERN Stack/Section-2/frontend/src/components/Todo.jsx
@@ -1,21 +1,30 @@
 import React from 'react'
+import '../styling/todo.css'
 
 const Todo = () => {
-  return (
-    <div>
-        <div className="container">
-            <div className="card">
-                <div className="card-header">
 
-                </div>
+    const addTodo = (e) => {
+        if(e.code === 'Enter'){
+            console.log("task added");
+        }
+    }
+    
+    return (
+        <div>
+            <h1 className='text-center display-4 fw-bold my-4 myh1'>To-Do App</h1>
+            <div className="container">
+                <div className="card">
+                    <div className="card-header">
+                        <input type="text" name="" id="" className='form-control' onKeyDown={addTodo}/>
+                    </div>
 
-                <div className="card-body">
-                    
+                    <div className="card-body">
+
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Todo
